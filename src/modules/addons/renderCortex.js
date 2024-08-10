ac2085d6323b3c579521d7120802b78def7c0646
lootoe-company <870334919@qq.com>
2024-08-10 17:20:13 +0800

    update cortex mat

--- src/modules/addons/renderCortex.js
+++ src/modules/addons/renderCortex.js
@@ -33,21 +33,23 @@ varying vec3 vNormal;
 varying vec3 vPositionNormal;
 void main() 
 {
-  float a = pow( b + s * abs(dot(vNormal, vPositionNormal)), p );
-  gl_FragColor = vec4( glowColor, a );
+  float intensity = pow( b + s * abs(dot(vNormal, vPositionNormal)), p );
+  
+  // 将轮廓颜色应用到片元上
+  gl_FragColor = vec4( glowColor, intensity );
 }
 `
 
 const brainMaskMaterial = new THREE.ShaderMaterial({
   uniforms: {
-    s: { type: 'f', value: -1 },
-    b: { type: 'f', value: 0.8 },
-    p: { type: 'f', value: 4 },
+    s: { type: 'f', value: -1.0 }, // 增加s值以增强轮廓的对比度
+    b: { type: 'f', value: 0.6 }, // 调整b值以更好地控制轮廓的亮度
+    p: { type: 'f', value: 2.0 }, // 调整p值以控制轮廓的渐变效果
     glowColor: { type: 'c', value: new THREE.Color('#2ff') },
   },
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
-  side: THREE.FrontSide,
+  side: THREE.BackSide, // 使用BackSide只显示物体的外部轮廓
   blending: THREE.AdditiveBlending,
   depthWrite: false,
 })
