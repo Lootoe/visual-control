6c82ed8794faf473ecb798c5b44380ea73b72258
lootoe-company <870334919@qq.com>
2024-08-12 09:21:31 +0800

    adjustCameraPosition

--- src/business/index.js
+++ src/business/index.js
@@ -1,7 +1,9 @@
 import { brainSyncRotate } from './brainSyncRotate'
 import { displayClickedNucleusName } from './displayClickedNucleusName'
+import { adjustCameraPosition } from './adjustCameraPosition'
 
 export const init3DHooks = () => {
   brainSyncRotate()
   displayClickedNucleusName()
+  adjustCameraPosition()
 }
