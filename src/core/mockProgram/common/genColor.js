b4560a9249d5288c1afa613ae6652b39e3426c02
lootoe-company <870334919@qq.com>
2024-08-15 10:01:00 +0800

    changeColor

--- src/core/mockProgram/common/genColor.js
+++ src/core/mockProgram/common/genColor.js
@@ -1,9 +1,4 @@
-export const genColor = (node, flag = false) => {
-  // 0\1\2\2'
-  if (node === 2 && flag) {
-    // P2的负极是黄色
-    return colors[3]
-  }
-  const colors = ['#27386f', '#0cce16', '#088fe9', '#f0ec05']
+export const genColor = (node) => {
+  const colors = ['#27386f', '#0cce16', '#ff6fe9']
   return colors[node]
 }
