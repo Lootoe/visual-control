9d2c8599a3e32e17a8adddbdc50a3aaea7f1abc9
lootoe-company <870334919@qq.com>
2024-08-16 16:55:05 +0800

    reset file

--- src/modules/addons/brainSyncRotate.js
+++ src/modules/addons/brainSyncRotate.js
@@ -8,23 +8,16 @@ const changeHeadSide = (mainSceneManager, assistSceneManager) => {
   const mainCamera = mainSceneManager.camera
   const assistCamera = assistSceneManager.camera
   const assistScreenDistance = assistSceneManager.config.screenDistance
+  const rotation = mainCamera.rotation
   const position = mainCamera.position
   const positionNormal = position.clone().normalize().multiplyScalar(assistScreenDistance)
-  const v = 0.0001
-  if (positionNormal.x < v) {
-    positionNormal.x = 0
-  }
-  if (positionNormal.y < v) {
-    positionNormal.y = 0
-  }
-  if (positionNormal.z < v) {
-    positionNormal.z = 0
-  }
+  assistCamera.rotation.z = rotation.z
+  assistCamera.rotation.y = rotation.y
+  assistCamera.rotation.x = rotation.x
   // !这里的同步存在小问题:主视图相机移动时，辅助视图相机也会移动，导致逃出俯视图UI框
   assistCamera.position.z = positionNormal.z
   assistCamera.position.y = positionNormal.y
   assistCamera.position.x = positionNormal.x
-  assistCamera.lookAt(0, 0, 0)
 }
 
 /** 同步主视图和辅视图的旋转 */
