d88d574aa313af3ec3123a945a5104841af16b9d
lootoe-company <870334919@qq.com>
2024-10-31 16:16:14 +0800

    'bugfix'

--- src/pages/Debug/Comsol/Index.vue
+++ src/pages/Debug/Comsol/Index.vue
@@ -43,40 +43,41 @@ const onFileSuccess = (file) => {
 <template>
   <control-scene></control-scene>
   <amp-slider @changeAmp="onAmpChanged"></amp-slider>
-  <el-upload
-    ref="uploadRef"
-    class="upload-demo"
-    :auto-upload="false"
-    :show-file-list="false"
-    :on-change="onFileSuccess"
-  >
-    <template #trigger>
-      <el-button type="primary">select file</el-button>
-    </template>
-  </el-upload>
-  <el-switch
-    v-model="renderMode"
-    class="switch-mode"
-    inline-prompt
-    size="large"
-    style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
-    inactive-text="显示点云"
-    active-text="显示表面"
-    :inactive-value="0"
-    :active-value="1"
-    @change="onModeChange"
-  />
+
+  <div class="panel">
+    <el-upload
+      ref="uploadRef"
+      class="upload-demo"
+      :auto-upload="false"
+      :show-file-list="false"
+      :on-change="onFileSuccess"
+    >
+      <template #trigger>
+        <el-button type="primary">select file</el-button>
+      </template>
+    </el-upload>
+    <el-switch
+      v-model="renderMode"
+      class="switch-mode"
+      inline-prompt
+      size="large"
+      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+      inactive-text="显示点云"
+      active-text="显示表面"
+      :inactive-value="0"
+      :active-value="1"
+      @change="onModeChange"
+    />
+  </div>
 </template>
 
 <style scoped lang="less">
-.upload-demo {
+.panel {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   position: absolute;
-  bottom: 2.4rem;
-  right: 0.34rem;
-}
-.switch-mode {
-  position: absolute;
-  bottom: 1.8rem;
-  right: 0.46rem;
+  bottom: 1.2rem;
+  right: 0.36rem;
 }
 </style>
